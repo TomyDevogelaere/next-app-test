726b5ef20d40085ea421eede9104513f5c9a73df
Tomy Devogelaere <tomy.devogelaere@telenet.be>
2024-05-28 15:10:15 +0200

    initial setup

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center">
+      <h1>This is an sample app with Nextjs</h1>
+    </main>
+  );
+}
